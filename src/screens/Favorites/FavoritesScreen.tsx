404a3d2b527f54f7b4694c72399768a883f35e53
Menna Mohie <mennaxmohiex@gmail.com>
2024-09-28 22:27:10 +0300

    handle no favs

--- src/screens/Favorites/FavoritesScreen.tsx
+++ src/screens/Favorites/FavoritesScreen.tsx
@@ -10,10 +10,21 @@ const FavoritesScreen = () => {
     const { favorites } = useFavorites()
     const navigation = useNavigation<FavoritesScreenNavigationProp>()
     const favoriteAlbums = Array.from(favorites.values())
+    if (favoriteAlbums.length === 0) {
+        return <Text style={styles.noFavoritesText}>Try adding some fav♪! </Text>
+    }
     const handlePress = (album: Album) => {
         navigation.navigate('AlbumDetails', { album })
     }
     return <AlbumList albums={favoriteAlbums} onAlbumPress={handlePress}></AlbumList>
 }
 
+const styles = StyleSheet.create({
+    noFavoritesText: {
+        textAlign: 'center',
+        fontSize: 25,
+        color: '#888',
+        marginTop: 20,
+    },
+})
 export default FavoritesScreen
